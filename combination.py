70e39c198e903d511f967de29c39735751af74e3
Seiichi Inoue <d35inou108@gmail.com>
2019-02-12 08:18:55 +0900

    small change

--- combination.py
+++ combination.py
@@ -1,12 +1,12 @@
 def combination(n: int) -> list:
-	c = []
-	for i in range(1<<n):
-		if i == 0:
-			continue
-		l = []
-		for j in range(n):
-			if i&(1<<j) != 0:
-				l.append(j+1)
-		c.append(l)
-	return c
+    c = []
+    for i in range(1<<n):
+        if i == 0:
+            continue
+        l = []
+        for j in range(n):
+            if i&(1<<j) != 0:
+                l.append(j+1)
+        c.append(l)
+    return c
 
