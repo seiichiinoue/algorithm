890d677b5035e02a2293d06cc0bb737040ff1449
Seiichi Inoue <d35inou108@gmail.com>
2019-02-17 00:48:07 +0900

    added gcds.py

---
+++ gcds.py
@@ -0,0 +1,14 @@
+from functiools import reduce
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+def gcds(*numbers):
+    return reduce(gcd, numbers)
+
+if __name__ == '__main__':
+    print(gcds(*[int(i) for i in input().split()]))
+    
