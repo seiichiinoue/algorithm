3585e3951856d5513bfafdf9a001afdf8a6ef8dc
Seiichi Inoue <d35inou108@gmail.com>
2019-09-01 00:40:52 +0900

    fix

--- number_theory/factorizer.cpp
+++ number_theory/factorizer.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> P;
+typedef pair<ll, ll> P;
+
+vector<P> factorizer(ll n) {
+    vector<P> facts;
+    for (ll p=2; p*p<=n; ++p) {
+        if (n % p != 0) continue;
+        int num = 0;
+        while (n % p == 0) { ++num; n /= p; }
+        facts.push_back(P(p, num));
+    }
+    if (n != 1) facts.push_back(P(n, 1));
+    return facts;
+}
 
 int main() {
-    vector<P> facts;
     int n; cin >> n;
-    int base=2, exp=0;
-    while (base*base<=n) {
-        while (n%base==0) {
-            n /= base; exp++;
-        }
-        if (exp>0) {
-            facts.push_back(make_pair(base, exp));
-        }
-        base++; exp=0;
-    }
-    if (n>1) facts.push_back(make_pair(n,1));
-    
-    for (int i=0; i<facts.size(); i++) {
-        cout << facts[i].first << " " << facts[i].second << endl;
+    auto res = factorizer(n);
+    for (auto prime_exp : res) {
+        cout << prime_exp.first << ": " << prime_exp.second << endl;
     }
 }
