1dcbeb0cb9c113f89969c40ca19fef7114d3a462
Seiichi Inoue <d35inou108@gmail.com>
2019-09-01 00:10:07 +0900

    fix

--- number_theory/divisor.cpp
+++ number_theory/divisor.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define rep(i, n) for(ll i=1LL; i<n; ++i)
 
 vector<ll> calc_divisor(ll n) {
     vector<ll> res;
-    rep(i, n) {
-        if (n%1==0) {
+    for (long long i=1LL; i*i<=n; ++i) {
+        if (n%i==0) {
             res.push_back(i);
             ll j = n / i;
             if (j != i) res.push_back(j);
