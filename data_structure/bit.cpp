0f0006a5085db4d7b925ebe3b2c26512566cd081
Seiichi Inoue <d35inou108@gmail.com>
2019-11-27 02:53:51 +0900

    updated bit

--- data_structure/bit.cpp
+++ data_structure/bit.cpp
@@ -14,19 +14,22 @@ constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 template <class Abel> struct BIT {
     const Abel UNITY_SUM = 0;
     vector<Abel> dat;
+    /* [1, n] */
     BIT(int n) : dat(n+1, UNITY_SUM) {}
     void init(int n) { dat.assign(n+1, UNITY_SUM); }
-
+    /* a is 1-indexed */
     inline void add(int a, Abel x) {
         for (int i=a; i<(int)dat.size(); i+=i & -i)
             dat[i] = dat[i] + x;
     }
+    /* [1, a], a is 1-indexed */
     inline Abel sum(int a) {
         Abel res = UNITY_SUM;
         for (int i=a; i>0; i-=i & -i)
             res = res + dat[i];
         return res;
     }
+    /* [a, b), a and b are 1-indexed */
     inline Abel sum(int a, int b) {
         return sum(b-1) - sum(a-1);
     }
